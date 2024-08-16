b99a88c150b6e061029d081e965645be03e00c01
Mottuqe Brid <mottuqebrid@gmail.com>
2024-08-16 13:55:52 +0600

    update

--- src/App.js
+++ src/App.js
@@ -5,13 +5,27 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>my first paragraph</p>
+        <Person name="Rubel Nayok"></Person>
+        <Person name="josim"></Person>
+        <Person name="Razzak"></Person>
       </header>
     </div>
   );
+}
+
+function Person(props) {
+  const personstyle = {
+    backgroundColor: 'blue',
+    color: '#fff'
+  }
+  console.log(props)
+  return (
+    <div style={{ border: '2px solid red', padding: '10px', margin: '10px' }}>
+      <h1 style={personstyle}>Name: {props.name}</h1>
+      <h3>Hero off the year</h3>
+    </div>
+  )
 }
 
 export default App;
